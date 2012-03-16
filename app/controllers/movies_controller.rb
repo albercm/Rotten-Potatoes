32be2f9542c966d4be394d1da55b9c53c3caff03
Alberto <saasbook@saasbook.(none)>
2012-03-16 08:57:15 -0700

    no code in viewsgedit & filter by rating and sort by title and date.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,31 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
-    @all_ratings =  ['G','PG','PG-13','R'] 
+    
+    if(@checked != nil)
+      @movies = @movies.find_all{ |m| @checked.has_key?(m.rating) and  @checked[m.rating]==true}
+    end
+    
+    if(params[:ratings] != nil)
+      @movies = @movies.find_all{ |m| params[:ratings].has_key?(m.rating) }
+    end
+    
+   if(params[:sort].to_s == 'title')
+    @movies = @movies.sort_by{|m| m.title }
+   elsif(params[:sort].to_s == 'release')
+    @movies = @movies.sort_by{|m| m.release_date.to_s }
+   end
+
+    @checked = {}
+    @all_ratings =  ['G','PG','PG-13','R']
+
+    @all_ratings.each { |rating|
+      if params[:ratings] == nil
+        @checked[rating] = false
+      else
+        @checked[rating] = params[:ratings].has_key?(rating)
+      end
+    }
   end
 
   def new
