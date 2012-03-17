0dd3dc64603eb4d9bb247f205cf2aff60798dd79
Alberto <saasbook@saasbook.(none)>
2012-03-17 03:35:41 -0700

    remember the settings (session)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,21 +7,43 @@ class MoviesController < ApplicationController
   end
 
   def index
+    
+ #    params.each { |p|
+ #       session[p] = p
+ #    }
+#      redirect_to movies_path(params => session)
+#    end
+    
     @movies = Movie.all
-    
+    @redirect = 0
     if(@checked != nil)
-      @movies = @movies.find_all{ |m| @checked.has_key?(m.rating) and  @checked[m.rating]==true}
+      @movies = @movies.find_all{ |m| @checked.has_key?(m.rating) and  @checked[m.rating]==true}      
     end
     
-    if(params[:ratings] != nil)
-      @movies = @movies.find_all{ |m| params[:ratings].has_key?(m.rating) }
-    end
     
    if(params[:sort].to_s == 'title')
+    session[:sort] = params[:sort]
     @movies = @movies.sort_by{|m| m.title }
    elsif(params[:sort].to_s == 'release')
+    session[:sort] = params[:sort]
     @movies = @movies.sort_by{|m| m.release_date.to_s }
+   elsif(session.has_key?(:sort) )
+    params[:sort] = session[:sort]
+    @redirect = 1
    end
+    
+    
+    if(params[:ratings] != nil)
+      session[:ratings] = params[:ratings]
+      @movies = @movies.find_all{ |m| params[:ratings].has_key?(m.rating) }
+    elsif(session.has_key?(:ratings) )
+      params[:ratings] = session[:ratings]
+      @redirect =1
+    end
+    
+    if(@redirect ==1)
+    redirect_to movies_path(:sort=>params[:sort], :ratings =>params[:ratings] )
+    end
 
     @checked = {}
     @all_ratings =  ['G','PG','PG-13','R']
